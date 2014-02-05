444d8ddd266d69687c928e609442c2e1a1c5d0de
23toys <milk41@gmail.com>
2014-02-05 17:43:13 -0200

    Version 2.1
    Added optional use of VPrediction, some fixes and tweaking.

--- Common/SidasAutoCarryPlugin - Ezreal.lua
+++ Common/SidasAutoCarryPlugin - Ezreal.lua
@@ -1,6 +1,6 @@
 ------#################################################################################------  ------########################### Ezreal's Time to strike! ############################------ ------###########################         by Toy           ############################------ ------#################################################################################------
 
---> Version: 2.0
+--> Version: 2.1
 
 --> Features:
 --> Prodictions in every skill, also taking their hitboxes in consideration.
@@ -15,10 +15,12 @@
 --> If Mystic Shot would interrupt the auto-attack, it waits till the auto-attack goes off before shoting it, therefore won't interrupt auto-attack(Reborn only, Revamped users won't be affected by this, but can still use the plugin normally).
 --> Optional use of FastCollision or not in the Range & Collision Settings menu (requires reload, it will display a message telling you if you are using Fast Collision or Normal Collision).
 --> Optional customizable skill ranges, set the skills to whatever range you like (default is max range), also you can change the skill cross-hair range (target selector range, if you set it to a value below 1100, Q and W will be limited by this range, recomended is betwen 1100 and 1200), disable the option to reset the ranges to the default, he ranges are configured with a slider.
+--> Optional use of VPrediction to predict the skillshoots instead of Prodictions (There's no need to reload too), with a slider to set the hitchance from 0 to 5, and a button to check the informations about hitchance ingame, VPrediction comes enabled by deafault, you can disable it on the Range & Libs menu.
 
 if myHero.charName ~= "Ezreal" then return end
 
 require "Prodiction"
+require "VPrediction"
  
 local qRange = 1100
 local wRange = 1050
@@ -39,6 +41,8 @@ local grab = nil
 
 local ProdictQCol = nil
 local ProdictQFastCol = nil
+
+local VP = nil
  
 function PluginOnLoad()
         AutoCarry.SkillsCrosshair.range = xRange
@@ -47,7 +51,9 @@ function PluginOnLoad()
         
         ProdictQ = Prodict:AddProdictionObject(_Q, qRange, 2000, 0.251, 80)
         ProdictW = Prodict:AddProdictionObject(_W, wRange, 1600, 0.25, 90)               
-        ProdictR = Prodict:AddProdictionObject(_R, rRange, 2000, 1, 150)                       
+        ProdictR = Prodict:AddProdictionObject(_R, rRange, 2000, 1, 150)
+        
+        VP = VPrediction()
 end
 
 function KS()
@@ -83,6 +89,16 @@ function PluginOnTick()
  AutoCarry.PluginMenu.ranges.qRanger = 1100
  AutoCarry.PluginMenu.ranges.wRanger = 1050
  AutoCarry.PluginMenu.ranges.rRanger = 2000
+ end
+ --
+ if AutoCarry.PluginMenu.ranges.HitChanceInfo then
+ PrintChat ("<font color='#FFFFFF'>Hitchance 0: No waypoints found for the target, returning target current position</font>")
+ PrintChat ("<font color='#FFFFFF'>Hitchance 1: Low hitchance to hit the target</font>")
+ PrintChat ("<font color='#FFFFFF'>Hitchance 2: High hitchance to hit the target</font>")
+ PrintChat ("<font color='#FFFFFF'>Hitchance 3: Target too slowed or/and too close(~100% hit chance)</font>")
+ PrintChat ("<font color='#FFFFFF'>Hitchance 4: Target inmmobile(~100% hit chace)</font>")
+ PrintChat ("<font color='#FFFFFF'>Hitchance 5: Target dashing(~100% hit chance)</font>")
+ AutoCarry.PluginMenu.ranges.HitChanceInfo = false
  end
  --
   if AutoCarry.PluginMenu.ToggleMuramana then MuramanaToggle() end
@@ -145,13 +161,16 @@ function PluginOnDraw()
 end
 
  function Menu()
-  local HKR = string.byte("32")
-  AutoCarry.PluginMenu:addSubMenu("-- [Range & Collision Settings] --", "ranges")
+  local HKR = string.byte("T")
+  AutoCarry.PluginMenu:addSubMenu("-- [Range & Libs Settings] --", "ranges")
   AutoCarry.PluginMenu.ranges:addParam("useRanger", "Use - Custom Ranges", SCRIPT_PARAM_ONOFF, true)
   AutoCarry.PluginMenu.ranges:addParam("xRanger", "Range - Skill Crosshair", SCRIPT_PARAM_SLICE, 1200, 550, 1500, 0)
   AutoCarry.PluginMenu.ranges:addParam("qRanger", "Range - Mystic Shot", SCRIPT_PARAM_SLICE, 1100, 550, 1100, 0)
   AutoCarry.PluginMenu.ranges:addParam("wRanger", "Range - Essence Flux", SCRIPT_PARAM_SLICE, 1050, 550, 1050, 0)
   AutoCarry.PluginMenu.ranges:addParam("rRanger", "Range - Trueshot Barrage", SCRIPT_PARAM_SLICE, 2000, 550, 2000, 0)
+  AutoCarry.PluginMenu.ranges:addParam("UseVP", "Use - VPrediction", SCRIPT_PARAM_ONOFF, true)
+  AutoCarry.PluginMenu.ranges:addParam("HitChance", "VP - Hitchance", SCRIPT_PARAM_SLICE, 2, 0, 5, 0)
+  AutoCarry.PluginMenu.ranges:addParam("HitChanceInfo", "Info - Hitchance", SCRIPT_PARAM_ONOFF, false)
   AutoCarry.PluginMenu.ranges:addParam("ColSwap", "Use - Fast Collision (Reload)", SCRIPT_PARAM_ONOFF, false)
   AutoCarry.PluginMenu:addParam("sep", "-- Ultimate Options --", SCRIPT_PARAM_INFO, "")
   AutoCarry.PluginMenu:addParam("useR", "Use - Trueshot Barrage", SCRIPT_PARAM_ONKEYDOWN, false, HKR)
@@ -189,34 +208,78 @@ function Checks()
 end
  
 function MysticShot()
-        if IsSACReborn and QAble and AutoCarry.PluginMenu.useQ then ProdictQ:GetPredictionCallBack(Target, CastQ)
+    if AutoCarry.PluginMenu.ranges.useVP then
+      for i, target in pairs(GetEnemyHeroes()) do
+      CastPosition,  HitChance,  Position = VP:GetLineCastPosition(Target, 0.251, 80, qRange, 2000, myHero)
+        if IsSACReborn and QAble and AutoCarry.PluginMenu.useQ and not AutoCarry.Orbwalker:IsShooting() and HitChance >= AutoCarry.PluginMenu.ranges.HitChance and GetDistance(CastPosition) < AutoCarry.PluginMenu.ranges.qRanger then CastSpell(_Q, CastPosition.x, CastPosition.z)
+        elseif QAble and AutoCarry.PluginMenu.useQ and HitChance >= AutoCarry.PluginMenu.ranges.HitChance and GetDistance(CastPosition) < AutoCarry.PluginMenu.ranges.qRanger then CastSpell(_Q, CastPosition.x, CastPosition.z) end
+      end
+    else
+        if IsSACReborn and QAble and AutoCarry.PluginMenu.useQ and not AutoCarry.Orbwalker:IsShooting() then ProdictQ:GetPredictionCallBack(Target, CastQ)
         elseif QAble and AutoCarry.PluginMenu.useQ then ProdictQ:GetPredictionCallBack(Target, CastQ) end
+    end
 end    
  
 function EssenceFlux()
+    if AutoCarry.PluginMenu.ranges.useVP then
+      for i, target in pairs(GetEnemyHeroes()) do
+      CastPosition,  HitChance,  Position = VP:GetLineCastPosition(Target, 0.25, 90, wRange, 1600, myHero)
+        if WAble and AutoCarry.PluginMenu.useW and not AutoCarry.PluginMenu.resetW and HitChance >= AutoCarry.PluginMenu.ranges.HitChance and GetDistance(CastPosition) < AutoCarry.PluginMenu.ranges.wRanger then CastSpell(_W, CastPosition.x, CastPosition.z) end
+      end
+    else
         if WAble and AutoCarry.PluginMenu.useW and not AutoCarry.PluginMenu.resetW then ProdictW:GetPredictionCallBack(Target, CastW) end
+    end
 end  
 
 function EssenceFluxReset()
-        if WAble and AutoCarry.Orbwalker:IsAfterAttack() and AutoCarry.PluginMenu.useW and AutoCarry.PluginMenu.resetW then ProdictW:GetPredictionCallBack(Target, CastW) end
+    if AutoCarry.PluginMenu.ranges.useVP then
+      for i, target in pairs(GetEnemyHeroes()) do
+      CastPosition,  HitChance,  Position = VP:GetLineCastPosition(Target, 0.25, 90, wRange, 1600, myHero)
+        if IsSACReborn and WAble and AutoCarry.Orbwalker:IsAfterAttack() and AutoCarry.PluginMenu.useW and AutoCarry.PluginMenu.resetW and HitChance >= 2 and GetDistance(CastPosition) < AutoCarry.PluginMenu.ranges.wRanger then CastSpell(_W, CastPosition.x, CastPosition.z) end
+      end
+    else
+        if IsSACReborn and WAble and AutoCarry.Orbwalker:IsAfterAttack() and AutoCarry.PluginMenu.useW and AutoCarry.PluginMenu.resetW then ProdictW:GetPredictionCallBack(Target, CastW) end
+    end
+end
+
+function OnAttacked()
+        if not IsSACReborn and WAble and AutoCarry.MainMenu.AutoCarry and AutoCarry.PluginMenu.useW and AutoCarry.PluginMenu.resetW then ProdictW:GetPredictionCallBack(Target, CastW) end
 end
 
 function MysticShot2()
-        if IsSACReborn and QAble and AutoCarry.PluginMenu.useQ2 then ProdictQ:GetPredictionCallBack(Target, CastQ)
+    if AutoCarry.PluginMenu.ranges.useVP then
+      for i, target in pairs(GetEnemyHeroes()) do
+      CastPosition,  HitChance,  Position = VP:GetLineCastPosition(Target, 0.251, 80, qRange, 2000, myHero)
+        if IsSACReborn and QAble and AutoCarry.PluginMenu.useQ2 and not AutoCarry.Orbwalker:IsShooting() and HitChance >= AutoCarry.PluginMenu.ranges.HitChance and GetDistance(CastPosition) < AutoCarry.PluginMenu.ranges.qRanger then CastSpell(_Q, CastPosition.x, CastPosition.z)
+        elseif QAble and AutoCarry.PluginMenu.useQ2 and HitChance >= AutoCarry.PluginMenu.ranges.HitChance and GetDistance(CastPosition) < AutoCarry.PluginMenu.ranges.qRanger then CastSpell(_Q, CastPosition.x, CastPosition.z) end
+      end
+    else
+        if IsSACReborn and QAble and AutoCarry.PluginMenu.useQ2 and not AutoCarry.Orbwalker:IsShooting() then ProdictQ:GetPredictionCallBack(Target, CastQ)
         elseif QAble and AutoCarry.PluginMenu.useQ2 then ProdictQ:GetPredictionCallBack(Target, CastQ) end
+    end
 end    
  
 function EssenceFlux2()
-        if WAble and AutoCarry.PluginMenu.useW2 then ProdictW:GetPredictionCallBack(Target, CastW) end
-end  
-
-function MysticShot3()
-        if QAble and AutoCarry.PluginMenu.qFarm then ProdictQ:GetPredictionCallBack(Minion, CastQ) end
-end    
+    if AutoCarry.PluginMenu.ranges.useVP then
+      for i, target in pairs(GetEnemyHeroes()) do
+      CastPosition,  HitChance,  Position = VP:GetLineCastPosition(Target, 0.25, 90, wRange, 1600, myHero)
+        if WAble and AutoCarry.PluginMenu.useW2 and not AutoCarry.PluginMenu.resetW and HitChance >= AutoCarry.PluginMenu.ranges.HitChance and GetDistance(CastPosition) < AutoCarry.PluginMenu.ranges.wRanger then CastSpell(_W, CastPosition.x, CastPosition.z) end
+      end
+    else
+        if WAble and AutoCarry.PluginMenu.useW2 and not AutoCarry.PluginMenu.resetW then ProdictW:GetPredictionCallBack(Target, CastW) end
+    end
+end   
 
 function TrueShotBarrage()
+    if AutoCarry.PluginMenu.ranges.useVP then
+      for i, target in pairs(GetEnemyHeroes()) do
+      CastPosition,  HitChance,  Position = VP:GetLineCastPosition(Target, 1, 150, rRange, 2000, myHero)
+        if Target and RAble and HitChance >= AutoCarry.PluginMenu.ranges.HitChance and GetDistance(CastPosition) < AutoCarry.PluginMenu.ranges.rRanger then CastSpell(_R, CastPosition.x, CastPosition.z) end
+      end
+    else
         if Target and RAble then ProdictR:GetPredictionCallBack(Target, CastR) end
-end    
+    end
+end
  
 local function getHitBoxRadius(target)
         return GetDistance(target, target.minBBox)
